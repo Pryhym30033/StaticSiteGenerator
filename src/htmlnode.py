b7ab36da1cb9a972e6801085dad437025557d54d
Pryhym30033 <pryhymcapitalmanagement@gmail.com>
2025-03-07 19:24:10 -0500

    parent node without testing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,4 +40,29 @@ class LeafNode(HTMLNode):
                 htmlProp = self.props_to_html()
                 return f"<{self.tag}{htmlProp}>{self.value}</{self.tag}>" 
         
-    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+
+    def to_html(self):
+
+        if self.tag is None: 
+            raise ValueError("Tag is required for ParentNode")
+        elif self.children is None or len(self.children)<1:
+            raise ValueError("Children are required for ParentNode")
+        else:
+            output = f"<{self.tag}"
+
+            if self.props is not None:  
+                for key in self.props.keys():
+                    output += f" {key}=\"{self.props[key]}\""
+            output += ">"
+            
+
+            for child in self.children:
+                output += child.to_html()
+
+            output += f"</{self.tag}>"
+
+            return output
+
